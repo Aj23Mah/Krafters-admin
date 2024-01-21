17693284dd7b3662f01ee1bd37077086ef116038
Aj23Mah <ajaymaharjan758@gmail.com>
2024-01-21 15:46:07 +0545

    add

--- src/components/pages/Student.tsx
+++ src/components/pages/Student.tsx
@@ -10,11 +10,11 @@ import user2 from "../../assets/images/user2.png";
 import user3 from "../../assets/images/user3.png";
 import user4 from "../../assets/images/user4.png";
 import { Dots } from "tabler-icons-react";
-import { Eye } from 'tabler-icons-react';
+import { Eye } from "tabler-icons-react";
 
 const Student = () => {
   const navigate = useNavigate();
-  
+
   const [openDropdown, setOpenDropdown] = useState(null);
 
   const dropdown = (index) => {
@@ -30,8 +30,7 @@ const Student = () => {
       imgURL: user1,
       name: "Leslie Alexander",
       course: "	Vuejs Courses",
-      // batch: {new Date().toLocaleString() + ""},
-      batch: "1:00 p.m.",
+      batch: "1:00 p.m.", // {new Date().toLocaleString() + ""}
       phone: "0283-555-0029",
     },
     {
@@ -55,19 +54,27 @@ const Student = () => {
       batch: "1:00 p.m.",
       phone: "0283-555-0029",
     },
-  ]
+  ];
 
-  const [rowStates, setRowStates] = useState(StudentData.map(() => ({ isOpen: false, selectedOption: "Status..." })));
+  const [rowStates, setRowStates] = useState(
+    StudentData.map(() => ({ isOpen: false, selectedOption: "Status..." }))
+  );
 
   const toggleDropdown = (index) => {
     setRowStates((prevStates) =>
-      prevStates.map((state, i) => (i === index ? { ...state, isOpen: !state.isOpen } : state))
+      prevStates.map((state, i) =>
+        i === index ? { ...state, isOpen: !state.isOpen } : state
+      )
     );
   };
 
   const handleOptionClick = (index, option) => {
     setRowStates((prevStates) =>
-      prevStates.map((state, i) => (i === index ? { ...state, selectedOption: option, isOpen: false } : state))
+      prevStates.map((state, i) =>
+        i === index
+          ? { ...state, selectedOption: option, isOpen: false }
+          : state
+      )
     );
   };
 
@@ -113,71 +120,73 @@ const Student = () => {
             <td>Status</td>
             <td>Action</td>
           </tr>
-            {StudentData.map((v, index) => (
-          <tr key={index} className="text-md even:bg-gray-100 odd:bg-white">
+          {StudentData.map((v, index) => (
+            <tr key={index} className="text-md even:bg-gray-100 odd:bg-white">
               <td className="flex items-center gap-sm">
-              <p>
-                <img src={v.imgURL} alt="" height={64} width={64} />
-              </p>
-              <p>{v.name}</p>
-            </td>
-            <td>{v.course}</td>
-            <td>{v.batch}</td>
-            <td>{v.phone}</td>
-            <td>
-              <div className="relative inline-block text-left">
-                <div>
-                  <button
-                    type="button"
-                    onClick={() => toggleDropdown(index)}
-                    className="inline-flex justify-center w-[120px] rounded-md border-none shadow-sm px-sm py-xs bg-slate-200 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:bg-gray-200"
-                  >
-                    {rowStates[index].selectedOption}
-                    <svg
-                      className="-mr-[4px] ml-xs h-[20px] w-[20px]"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
+                <p>
+                  <img src={v.imgURL} alt="" height={64} width={64} />
+                </p>
+                <p>{v.name}</p>
+              </td>
+              <td>{v.course}</td>
+              <td>{v.batch}</td>
+              <td>{v.phone}</td>
+              <td>
+                <div className="relative inline-block text-left">
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => toggleDropdown(index)}
+                      className="inline-flex justify-center w-[120px] rounded-md border-none shadow-sm px-sm py-xs bg-slate-200 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:bg-gray-200"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                </div>
-
-                {rowStates[index].isOpen && (
-                  <div className="origin-top-right absolute right-none w-full z-10 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <div
-                      className="py-[4px]"
-                      role="menu"
-                      aria-orientation="vertical"
-                      aria-labelledby="options-menu"
-                    >
-                      {["Active", "In Active", "Pending", "Blocked"].map((option) => ( 
-                        <a
-                        key={option}
-                        href="#"
-                        className="block no-underline px-sm py-xs text-base text-gray-700 hover:bg-gray-100"
-                        role="menuitem"
-                        onClick={() => handleOptionClick(index, option)}
+                      {rowStates[index].selectedOption}
+                      <svg
+                        className="-mr-[4px] ml-xs h-[20px] w-[20px]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
                       >
-                        {option}
-                      </a>
-                       ))}
-                    </div>
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
                   </div>
-                )}
-              </div>
-            </td>
-            <td className="flex items-center gap-xs">
-              <p onClick={() => navigate("/edit-student")}>
-                <Edit size={28} strokeWidth={2} />
-              </p>
-              <p>
-                <div className="relative inline-block">
+
+                  {rowStates[index].isOpen && (
+                    <div className="origin-top-right absolute right-none w-full z-10 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                      <div
+                        className="py-[4px]"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        {["Active", "In Active", "Pending", "Blocked"].map(
+                          (option) => (
+                            <a
+                              key={option}
+                              href="#"
+                              className="block no-underline px-sm py-xs text-base text-gray-700 hover:bg-gray-100"
+                              role="menuitem"
+                              onClick={() => handleOptionClick(index, option)}
+                            >
+                              {option}
+                            </a>
+                          )
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </td>
+              <td className="flex items-center gap-xs">
+                <p onClick={() => navigate("/edit-student")}>
+                  <Edit size={28} strokeWidth={2} />
+                </p>
+                <p>
+                  <div className="relative inline-block">
                     <button
                       onClick={() => dropdown(index)}
                       className="text-black px-sm bg-transparent py-[4px] rounded focus:outline-none border-none"
@@ -191,16 +200,17 @@ const Student = () => {
                             <Trash size={24} strokeWidth={2} /> <p>Delete</p>
                           </li>
                           <li className="hover:bg-gray-300 flex items-center gap-xs">
-                            <Eye size={24} strokeWidth={2} /> <p>View Details</p>
+                            <Eye size={24} strokeWidth={2} />{" "}
+                            <p>View Details</p>
                           </li>
                         </ul>
                       </div>
                     )}
                   </div>
-              </p>
-            </td>
-          </tr>
-            ))}
+                </p>
+              </td>
+            </tr>
+          ))}
         </table>
       </div>
     </div>
